44593a33361ff7a029a38ae621828a39564e6c45
Fabian Rausch <fabian.rausch@pwc.com>
2022-04-01 00:03:40 +0200

    Implement static dictionary for POS tag to id mapping

--- spert/util.py
+++ spert/util.py
@@ -223,3 +223,6 @@ def check_version(config, model_class, model_path):
                    % (model_class.VERSION, loaded_version))
             msg += "Use the code matching your version or train a new model."
             raise Exception(msg)
+
+def get_pos_dict():
+    return {'ADJ': 0, 'ADP': 1, 'ADV': 2, 'AUX': 3, 'CCONJ': 4, 'DET': 5, 'INTJ': 6, 'NOUN': 7, 'NUM': 8, 'PART': 9, 'PRON': 10, 'PROPN': 11, 'PUNCT': 12, 'SCONJ': 13, 'SYM': 14, 'VERB': 15, 'X': 16}
